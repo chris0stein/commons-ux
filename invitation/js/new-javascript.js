ce3dbde428f61ddf248d412f254aef92690c18d1
profstein <profstein@users.noreply.github.com>
2018-04-20 04:20:55 -0400

    updated tab styling and related html js. similar to commons profile tabs

--- invitation/js/new-javascript.js
+++ invitation/js/new-javascript.js
@@ -16,3 +16,23 @@ var formPart = document.querySelectorAll(".form-part");
         }, this);
     });
 });
+
+
+//Michael's tab script modified to work with cacap styling
+// sorry, didn't have time to use the same code as tabs on portfolio pages.
+var cacapTab = document.querySelectorAll(".cacap-tab");
+var cacapPart = document.querySelectorAll(".cacap-part");
+
+[].forEach.call(cacapTab, function(tab, i, cacapTab) {
+    tab.addEventListener('click', function() {
+        [].forEach.call(cacapTab, function(tab, i) {
+            if(tab !== this) {
+                tab.classList.remove("current-tab");
+                cacapPart[i].classList.add("not-visible");
+            } else {
+                tab.classList.add("current-tab");
+                cacapPart[i].classList.remove("not-visible");
+            }
+        }, this);
+    });
+});
